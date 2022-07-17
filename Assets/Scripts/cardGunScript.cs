e75a0e3fd8ab9b118ec2fec29c34f5c7a7b2f111
Peter <37885818+baiepm1@users.noreply.github.com>
2022-07-17 04:34:25 -0400

    card weapon, laser weapon, fireball

---
+++ Assets/Scripts/cardGunScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardGunScript : MonoBehaviour
+{
+    public GameObject bulletPrefab1;
+    public GameObject bulletPrefab2;
+    public GameObject bulletPrefab3;
+    public GameObject bulletPrefab4;
+}
